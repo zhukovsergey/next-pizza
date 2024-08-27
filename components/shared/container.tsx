c46b541e52992cdecc01777714b82d301ea19304
zhukovsergey <zhukov@etalon48.com>
2024-08-27 09:16:29 +0300

    header component

---
+++ components/shared/container.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+interface Props {
+  className?: string;
+}
+
+export const Container: React.FC<React.PropsWithChildren<Props>> = ({
+  className,
+  children,
+}) => {
+  return (
+    <div className={cn("mx-auto max-w-[1280px]", className)}>{children}</div>
+  );
+};
