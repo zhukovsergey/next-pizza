eb4b2e6855a1ed44445a5f055c4110199bdcddf2
Nastya <zhukov@etalon48.com>
2024-08-27 20:57:06 +0300

    05-23-51

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -4,14 +4,8 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   const query = req.nextUrl.searchParams.get('query') || '';
 
-  const products = await prisma.product.findMany({
-    where: {
-       
-        OR: [
-          { name: { contains: query } },
-        ]
-    },
-  });
+  const products = await prisma.product.findMany();
+  const prod = products.filter(product => product.name.toLowerCase().includes(query.toLowerCase()))
 
-  return NextResponse.json(products);
+  return NextResponse.json(prod);
 }
